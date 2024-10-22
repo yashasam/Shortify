d01d34bed171bfffc8dcec0c4efe40f8cdc90625
Yashaskabyar <yashasam2000@gmail.com>
2024-10-22 19:14:36 +0530

    Update Footer.jsx
    updated the file

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,8 +10,8 @@ const Footer = () => {
                     Shortify
                 </div>
                 <div className='flex gap-4 place-content-center mt-2 sm:mt-0'>
-                    <a href="https://github.com/GuruDarshan57" target="_blank"><i className="fa-brands fa-square-git text-2xl cursor-pointer" style={{ color: "#ffffff" }} ></i></a>
-                    <a href="https://www.linkedin.com/in/gurudarshan-l-772a1b25b/" target="_blank"><i className="fa-brands fa-linkedin text-2xl cursor-pointer" style={{ color: "#ffffff" }}></i></a>
+                    <a href="https://github.com/yashasam" target="_blank"><i className="fa-brands fa-square-git text-2xl cursor-pointer" style={{ color: "#ffffff" }} ></i></a>
+                    <a href="https://www.linkedin.com/in/yashasam/" target="_blank"><i className="fa-brands fa-linkedin text-2xl cursor-pointer" style={{ color: "#ffffff" }}></i></a>
                 </div>
                 <div className='flex mt-2 sm:mt-0 tracking-wider font-medium place-content-center'>
                     Copyright © {new Date().toString().slice(10, 15)} YASHAS A M
